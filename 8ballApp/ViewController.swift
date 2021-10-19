1b8dc23d758cf5dbed1db46464f11ca2d6ff2172
yardman-s <serhii.dvornyk@gmail.com>
2021-10-19 18:55:09 +0300

    Initial Commit

---
+++ 8ballApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  8ballApp
+//
+//  Created by Sergey Dvornik on 19.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
