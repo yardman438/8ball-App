3287745f71fc2cebbe58823ad0cc848b9591c5a8
Sergey Dvornyk <serhii.dvornyk@gmail.com>
2021-11-08 09:30:50 +0200

    Refactoring

--- 8ballApp/ViewControllers/MainViewController.swift
+++ 8ballApp/ViewControllers/MainViewController.swift
@@ -9,24 +9,31 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    // User interface elements
     @IBOutlet weak var answerLabel: UILabel!
     
     var answerManager = AnswerManager()
+    
+    //MARK: - View lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         answerManager.delegate = self
     }
     
+    // Sending the request by shaking the device
     override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         guard motion == .motionShake else { return }
         answerManager.fetchRandomAnswer()
     }
     
+    // User interaction with the interface
     @IBAction func shakeButtonPressed(_ sender: UIButton) {
         answerManager.fetchRandomAnswer()
     }
 }
+
+    //MARK: - Extensions
 
 extension MainViewController: AnswerManagerDelegate {
     func updateInterface(_: AnswerManager, with randomAnswer: RandomAnswer) {
